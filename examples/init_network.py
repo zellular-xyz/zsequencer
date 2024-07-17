41e72e8d8bc2808b4299438c27f6ac523a6f679c
Siftal <m.khanmohamadzadeh@gmail.com>
2024-07-17 21:21:24 +0330

    feat: add multi-app support and lock transactions state, format and clean code

--- examples/init_network.py
+++ examples/init_network.py
@@ -2,31 +2,31 @@ import asyncio
 import json
 import os
 import sys
-from typing import Any, Dict, List
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from typing import Any
 
 import requests
 from pyfrost.network.dkg import Dkg
 
-from config import zconfig
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from zsequencer.config import zconfig
 from zsequencer.sequencer import tss
 
 
 async def run_sample() -> None:
     nodes_info: tss.NodesInfo = tss.NodesInfo()
-    all_nodes: List[str] = nodes_info.get_all_nodes()
+    all_nodes: list[str] = nodes_info.get_all_nodes()
     dkg: Dkg = Dkg(nodes_info, default_timeout=50)
-    dk: Dict[str, Any] = await dkg.request_dkg(zconfig.THRESHOLD_NUMBER, all_nodes)
-    data: Dict[str, Any] = {
+    dk: dict[str, Any] = await dkg.request_dkg(zconfig.THRESHOLD_NUMBER, all_nodes)
+    data: dict[str, Any] = {
         "public_key": dk["public_key"],
         "public_shares": dk["public_shares"],
         "party": dk["party"],
     }
-    headers: Dict[str, str] = {"Content-Type": "application/json"}
     for node in zconfig.NODES.values():
         url: str = f'http://{node["host"]}:{node["port"]}/node/distributed_keys'
-        requests.put(url, json.dumps(data), headers=headers)
+        requests.put(url, json.dumps(data), headers=zconfig.HEADERS)
     print("Successfully initialized: ", dk)
 
 
