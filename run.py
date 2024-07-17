693e97a38d00ba6d2f76d55666de049c9c369376
Siftal <m.khanmohamadzadeh@gmail.com>
2024-07-17 21:25:23 +0330

    feat: add multi-app support and lock transactions state, format and clean code

--- run.py
+++ run.py
@@ -1,16 +1,22 @@
-import os
-import sys
+"""
+Main script to run Zellular Node and Sequencer tasks.
+"""
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import argparse
 import asyncio
+import logging
+import os
+import sys
 import threading
 import time
 
 from flask import Flask
 
-from config import zconfig
-from shared_state import state
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from zsequencer.common.db import zdb
+from zsequencer.config import zconfig
 from zsequencer.node import tasks as node_tasks
 from zsequencer.node.routes import node_blueprint
 from zsequencer.sequencer import tasks as sequencer_tasks
@@ -19,6 +25,7 @@ from zsequencer.sequencer.routes import sequencer_blueprint
 
 
 def create_app(tss_blueprint) -> Flask:
+    """Create and configure the Flask application."""
     app: Flask = Flask(__name__)
     app.secret_key = zconfig.SECRET_KEY
 
@@ -29,12 +36,13 @@ def create_app(tss_blueprint) -> Flask:
 
 
 def run_node_tasks() -> None:
+    """Periodically run node tasks."""
     while True:
         time.sleep(zconfig.SEND_TXS_INTERVAL)
         if zconfig.NODE["id"] == zconfig.SEQUENCER["id"]:
             continue
 
-        if state._pause_node.is_set():
+        if zdb.pause_node.is_set():
             continue
 
         node_tasks.send_txs()
@@ -42,35 +50,42 @@ def run_node_tasks() -> None:
 
 
 def run_sequencer_tasks() -> None:
+    """Run sequencer tasks in an asynchronous event loop."""
     loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(run_sequencer_tasks_async())
 
 
 async def run_sequencer_tasks_async() -> None:
+    """Asynchronously run sequencer tasks."""
     while True:
-        time.sleep(zconfig.SYNC_INTERVAL)
+        await asyncio.sleep(zconfig.SYNC_INTERVAL)
         if zconfig.NODE["id"] != zconfig.SEQUENCER["id"]:
             continue
 
-        if state._pause_node.is_set():
+        if zdb.pause_node.is_set():
             continue
 
         await sequencer_tasks.sync()
 
 
 def run_flask_app(app: Flask) -> None:
+    """Run the Flask application."""
+    # Set the logging level to WARNING to suppress INFO level logs
+    logger: logging.Logger = logging.getLogger("werkzeug")
+    logger.setLevel(logging.WARNING)
     app.run(
         host="localhost",
         port=zconfig.PORT,
-        debug=True,
+        debug=False,
         threaded=True,
         use_reloader=False,
     )
 
 
 def main(node_id) -> None:
-    tss_node = tss.gen_node(node_id)
+    """Main entry point for running the Zellular Node."""
+    tss_node: tss.Node = tss.gen_node(node_id)
     app: Flask = create_app(tss_node.blueprint)
 
     # Start periodic task in a thread
