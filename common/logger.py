929e759053b6f2bb8dc028807c7106d17e3fb83c
Siftal <m.khanmohamadzadeh@gmail.com>
2024-07-17 21:16:29 +0330

    style: format and clean the code

--- common/logger.py
+++ common/logger.py
@@ -1,10 +1,14 @@
+"""This module configures and sets up logging for the zellular application."""
+
 import logging
 from logging import FileHandler, Logger, StreamHandler
 
+# Set up the logger
 zlogger: Logger = logging.getLogger("zellular_logger")
 zlogger.setLevel(logging.DEBUG)
 
-LOG_COLORS = {
+# Define log colors for different levels
+LOG_COLORS: dict = {
     "DEBUG": "\033[94m",  # Blue
     "INFO": "\033[92m",  # Green
     "WARNING": "\033[93m",  # Yellow
@@ -13,6 +17,7 @@ LOG_COLORS = {
     "RESET": "\033[0m",  # Reset to default
 }
 
+# Define the standard formatter
 str_formatter: logging.Formatter = logging.Formatter(
     fmt="%(asctime)s - %(levelname)s - %(filename)s(%(lineno)d) - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
@@ -20,23 +25,25 @@ str_formatter: logging.Formatter = logging.Formatter(
 
 
 class ColoredFormatter(logging.Formatter):
-    def format(self, record):
-        log_level = record.levelname
-        record.msg = (
-            LOG_COLORS[log_level] + str_formatter.format(record) + LOG_COLORS["RESET"]
-        )
-        return record.msg
+    """Custom formatter to add colors to the log output based on the log level."""
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_level: str = record.levelname
+        record.msg = LOG_COLORS[log_level] + str(record.msg) + LOG_COLORS["RESET"]
+        return super().format(record)
 
 
+# Set up file handler
 file_handler: FileHandler = logging.FileHandler(filename="zellular.log", mode="w")
 file_handler.setFormatter(str_formatter)
 
+# Set up console handler with colored formatter
 console_handler: StreamHandler = logging.StreamHandler()
 console_handler.setFormatter(ColoredFormatter())
 
+# Add handlers to the logger
 zlogger.addHandler(file_handler)
 zlogger.addHandler(console_handler)
-
 
 if __name__ == "__main__":
     zlogger.debug("Debug message")
