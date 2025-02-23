ec06ecaed7e2201704d81ebb6b2f562852152c49
Ali Moosavi <amoosavi41@gmail.com>
2025-02-23 19:55:59 +0330

    Fix: Change version

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class NodeConfig(BaseSettings):
-    version: str = Field(default="v0.0.14")
+    version: str = Field(default="v0.0.15")
     nodes_info_sync_border: int = Field(default=5)
 
     nodes_source: str = Field(default="file")
