1a1417f38523cf32bf52e48fc90b05a0b499d213
Alireza Roshanzamir <a.roshanzamir1996@gmail.com>
2025-02-27 22:55:25 +0330

    Use better parameter names for the is_state_before_or_equal function.

--- common/state.py
+++ common/state.py
@@ -10,8 +10,8 @@ OPERATIONAL_STATES: Sequence[OperationalState] = ("sequenced", "locked", "finali
 STATES: Sequence[State] = ("initialized", *OPERATIONAL_STATES)
 
 
-def is_state_before_or_equal(first: State, other: State) -> bool:
-    return STATES.index(first) <= STATES.index(other)
+def is_state_before_or_equal(base_state: State, comparison_state: State) -> bool:
+    return STATES.index(base_state) <= STATES.index(comparison_state)
 
 
 def next_state_or_none(state: State) -> OperationalState | None:
