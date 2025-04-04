60409bb82da4f38d1c00c0d07abdfa74e1524a5f
Ali Moosavi <amoosavi41@gmail.com>
2025-04-04 19:53:01 +0330

    Fix: use MovingWindowRateLimiter limiter and kilobytes size of batches for rate limiting

--- node/rate_limit.py
+++ node/rate_limit.py
@@ -1,20 +1,23 @@
+import math
+
 from limits import RateLimitItemPerSecond
 from limits.storage import MemoryStorage
-from limits.strategies import FixedWindowRateLimiter
+from limits.strategies import MovingWindowRateLimiter
 
-from common.batch import Batch, get_batch_size_bytes
+from common.batch import Batch, get_batch_size_kb
 from config import zconfig
 
 # Initialize rate limiter storage
 storage = MemoryStorage()
-limiter = FixedWindowRateLimiter(storage)
+limiter = MovingWindowRateLimiter(storage)
 
 
 def try_acquire_self_node_rate_limit(batches: list[Batch]) -> bool:
-    new_additive_size = sum(get_batch_size_bytes(batch) for batch in batches)
+    # Todo: use bytes size to avoid use math.ceil
+    new_additive_size = math.ceil(sum(get_batch_size_kb(batch) for batch in batches))
 
     rate_limit = RateLimitItemPerSecond(
-        zconfig.node_send_limit_size_bytes_per_second,
+        zconfig.node_send_limit_size_kb_per_second,
         zconfig.PUSH_RATE_LIMIT_WINDOW_SECONDS
     )
 
--- sequencer/rate_limit.py
+++ sequencer/rate_limit.py
@@ -1,21 +1,24 @@
+import math
+
 from limits import RateLimitItemPerSecond
 from limits.storage import MemoryStorage
-from limits.strategies import FixedWindowRateLimiter
+from limits.strategies import MovingWindowRateLimiter
 
-from common.batch import Batch, get_batch_size_bytes
+from common.batch import Batch, get_batch_size_kb
 from config import zconfig
 
 # Initialize rate limiter storage
 storage = MemoryStorage()
-limiter = FixedWindowRateLimiter(storage)
+limiter = MovingWindowRateLimiter(storage)
 
 
 def try_acquire_other_nodes_rate_limit_quota(node_id: str, batches: list[Batch]) -> bool:
-    new_additive_size = sum(get_batch_size_bytes(batch) for batch in batches)
+    # Todo: use bytes size to avoid use math.ceil
+    new_additive_size = math.ceil(sum(get_batch_size_kb(batch) for batch in batches))
 
     # Create a new rate limit dynamically each time
     rate_limit = RateLimitItemPerSecond(
-        zconfig.node_send_limit_size_bytes_per_second,
+        zconfig.node_send_limit_size_kb_per_second,
         zconfig.PUSH_RATE_LIMIT_WINDOW_SECONDS
     )
 
