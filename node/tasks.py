855180b0585f01127340be340bf3f7b82facf707
Ali Moosavi <amoosavi41@gmail.com>
2025-04-25 16:27:56 +0330

    Feat: send_app_batches_iteration

--- node/tasks.py
+++ node/tasks.py
@@ -52,15 +52,12 @@ def send_batches() -> None:
 
 def send_app_batches_iteration(app_name: str) -> bool:
     response = send_app_batches(app_name).get("data", {})
-    sequencer_last_finalized_hash = response.get("finalized", {}).get("hash", "")
-    finish_condition = (
-            not sequencer_last_finalized_hash
-            or zdb.get_batch_record_by_hash_or_empty(
-        app_name,
-        sequencer_last_finalized_hash,
-    )
-    )
-    return finish_condition
+    sequencer_last_finalized_index = (response
+                                      .get("finalized", {})
+                                      .get("index", BatchSequence.BEFORE_GLOBAL_INDEX_OFFSET))
+    if sequencer_last_finalized_index == BatchSequence.BEFORE_GLOBAL_INDEX_OFFSET:
+        return True
+    return sequencer_last_finalized_index <= zdb.get_last_operational_batch_record_or_empty(app_name).get("index")
 
 
 def send_app_batches(app_name: str) -> dict[str, Any]:
