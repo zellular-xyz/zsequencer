fd6223726e760d17a548722ea799362e18d24ce3
abramsymons <abramsymons@gmail.com>
2025-05-01 13:15:44 +0330

    upgrade to 0.0.18

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ class SequencerSabotageSimulation(BaseSettings):
 
 
 class NodeConfig(BaseSettings):
-    version: str = Field(default="v0.0.17")
+    version: str = Field(default="v0.0.18")
     nodes_info_sync_border: int = Field(default=5)
 
     nodes_source: str = Field(default="file")
