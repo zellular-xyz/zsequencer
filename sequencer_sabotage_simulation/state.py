b520a6be274a674dad86fa8b9d5f4176638e153e
Ali Moosavi <amoosavi41@gmail.com>
2025-05-03 15:06:47 +0330

    Feat: Implement sabotage time series dynamic config for network nodes

--- sequencer_sabotage_simulation/state.py
+++ sequencer_sabotage_simulation/state.py
@@ -2,37 +2,41 @@ import threading
 import time
 
 from config import zconfig
+from sequencer_sabotage_simulation.utils import get_node_sabotage_config
 from settings import SequencerSabotageSimulation
 
 
 class SequencerSabotageSimulationState:
     _instance = None
 
-    def __init__(self, conf: SequencerSabotageSimulation):
+    def __init__(self, conf: SequencerSabotageSimulation, address: str):
         self._conf = conf
-        self._out_of_reach = False
+        self._address = address
+        self._sabotage_time_series_conf = get_node_sabotage_config(config_file_path=conf.timeseries_nodes_state_file,
+                                                                   address=self._address)
+        self._out_of_reach = False if len(self._sabotage_time_series_conf) == 0 else (not self._sabotage_time_series_conf[0]['up'])
         self._stop_event = threading.Event()  # To allow stopping the thread gracefully
 
     @staticmethod
-    def get_instance(conf: SequencerSabotageSimulation):
+    def get_instance(conf: SequencerSabotageSimulation, address: str):
         if not SequencerSabotageSimulationState._instance:
-            SequencerSabotageSimulationState._instance = (
-                SequencerSabotageSimulationState(conf=conf)
-            )
+            SequencerSabotageSimulationState._instance = SequencerSabotageSimulationState(conf,
+                                                                                          address)
         SequencerSabotageSimulationState._instance.start_simulating()
         return SequencerSabotageSimulationState._instance
 
     def _simulate_out_of_reach(self):
         """Daemon thread function to periodically check the condition."""
+        current_state_index = 0
         while not self._stop_event.is_set():
-            if zconfig.is_sequencer and self._conf.out_of_reach_simulation:
-                # Simulate a delay to represent "out of reach" duration
-                self._out_of_reach = False
-                time.sleep(self._conf.in_reach_seconds)
-                self._out_of_reach = True
-                time.sleep(self._conf.out_of_reach_seconds)
-            # Sleep for a short interval before checking again
-            time.sleep(0.01)  # Adjust this interval as needed
+            if zconfig.is_sequencer:
+                # Todo: should also check this condition
+                # and self._sabotage_time_series_conf.out_of_reach_simulation
+                time.sleep(self._sabotage_time_series_conf[current_state_index]['time_duration'])
+                self._out_of_reach = not self._out_of_reach
+                current_state_index += 1
+                if current_state_index == len(self._sabotage_time_series_conf):
+                    break
 
     def start_simulating(self):
         """Start the daemon thread to monitor the condition."""
@@ -50,6 +54,5 @@ class SequencerSabotageSimulationState:
         return self._out_of_reach
 
 
-sequencer_sabotage_simulation_state = SequencerSabotageSimulationState.get_instance(
-    conf=SequencerSabotageSimulation(),
-)
+sequencer_sabotage_simulation_state = SequencerSabotageSimulationState.get_instance(conf=SequencerSabotageSimulation(),
+                                                                                    address=zconfig.ADDRESS)
