c5b96d5c1b6bab88bbd926e6766d71325d696106
abramsymons <abramsymons@gmail.com>
2025-05-13 18:16:42 +0330

    improve initing sequencer

--- config.py
+++ config.py
@@ -204,26 +204,23 @@ class Config:
 
     def init_sequencer(self) -> None:
         """Finds the initial sequencer id."""
-        current_network_nodes = self.HISTORICAL_NETWORK_STATE[
-            self.NETWORK_STATUS_TAG
-        ].nodes
+        attesting_nodes = self.last_state.attesting_nodes
+        sequencing_nodes = self.last_state.sequencing_nodes
+
         total_stake = self.HISTORICAL_NETWORK_STATE[self.NETWORK_STATUS_TAG].total_stake
 
-        sequencers_stake: dict[str, Any] = dict.fromkeys(
-            list(current_network_nodes.keys()), 0
-        )
-        for node_id in list(current_network_nodes.keys()):
+        sequencers_stake: dict[str, Any] = dict.fromkeys(sequencing_nodes, 0)
+        for node_id in attesting_nodes:
             if node_id == self.NODE["id"]:
                 continue
-            url: str = f"{current_network_nodes[node_id]['socket']}/node/state"
+            url: str = f"{attesting_nodes[node_id]['socket']}/node/state"
             try:
                 response = requests.get(url=url, headers=self.HEADERS, timeout=1).json()
                 if response["data"]["version"] != self.VERSION:
                     continue
                 sequencer_id = response["data"]["sequencer_id"]
-                sequencers_stake[sequencer_id] += current_network_nodes[node_id][
-                    "stake"
-                ]
+                if sequencer_id in sequencing_nodes:
+                    sequencers_stake[sequencer_id] += attesting_nodes[node_id]["stake"]
             except Exception:
                 zlogger.warning(f"Unable to get state from {node_id}")
         max_stake_id = max(sequencers_stake, key=lambda k: sequencers_stake[k])
