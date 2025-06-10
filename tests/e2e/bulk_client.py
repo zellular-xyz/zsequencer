30b834534b2fecbce9d934d52c3d88045bcf5995
Mahdi Heydari <36688816+abramsymons@users.noreply.github.com>
2025-06-10 10:09:56 +0330

    enable tps test (#164)

--- tests/e2e/bulk_client.py
+++ tests/e2e/bulk_client.py
@@ -10,14 +10,24 @@ BULK_SIZE = 500_000
 
 
 def main() -> None:
-    gateway = "http://localhost:6008/node/batches"
-    t = int(time.time())
+    gateway = "http://localhost:6008"
+    first = requests.get(gateway + "/node/simple_app/batches/finalized/last").json()
+    first_index = first["data"]["index"] if first["data"] else 0
     txs = [
-        json.dumps({"tx_id": str(uuid4()), "operation": "foo", "t": t})
+        json.dumps({"tx_id": str(uuid4()), "operation": "foo"})
         for i in range(BULK_SIZE)
     ]
-    r = requests.put(gateway, json={"simple_app": txs})
-    zlogger.info(r.text)
+    start = time.time()
+    r = requests.put(gateway + "/node/batches", json={"simple_app": txs})
+    zlogger.info(f"transactions sent: {r.text}")
+    while True:
+        time.sleep(0.05)
+        last = requests.get(gateway + "/node/simple_app/batches/finalized/last").json()
+        duration = time.time() - start
+        zlogger.info(f"duration: {duration:.2f}, last: {last}")
+        if last["data"] and last["data"]["index"] - first_index >= BULK_SIZE:
+            zlogger.info(f"TPS: {int(BULK_SIZE / duration)}")
+            break
 
 
 if __name__ == "__main__":
